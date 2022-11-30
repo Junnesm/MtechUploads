39e9c277983b6c3e657daff4b5f326b8654e2b21
Junne Murdock <junnemurdock@Junnes-MacBook-Pro.local>
2022-11-30 10:17:17 -0700

    adding Itunessearch

---
+++ Contest/Contest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Contest
+//
+//  Created by Junne Murdock on 11/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
